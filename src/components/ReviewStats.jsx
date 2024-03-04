f32172385a49cd5ac1bb0196b2e3d0080f9d58f5
Ayodele Aransiola <leomdevs@gmail.com>
2024-03-04 11:48:42 +0100

    feat: add average of the total review rating

--- src/components/ReviewStats.jsx
+++ src/components/ReviewStats.jsx
@@ -1,11 +1,17 @@
 
 
 function ReviewStats({reviews}) {
+    // sum of the total reviews rating and divided by the total length of the array and the output is converted to a single decimal place.
+
+    let average = (reviews.reduce((acc, cur) => {
+        return acc + cur.rating}, 0)/reviews.length).toFixed(1)
 
   return (
     <>
       <div className="review-stats">
-        <p>{reviews.length} Reviews</p>
+        <p>Reviews: {reviews.length}</p>
+
+        <p>Average: {isNaN(average) ? 0 : average}</p>
       </div>
     </>
   )
