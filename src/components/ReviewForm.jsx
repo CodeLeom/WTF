9165da73657b88d5b0dbca7cf80b804d319412ef
Ayodele Aransiola <leomdevs@gmail.com>
2024-03-05 12:10:07 +0100

    chore: add validation for text input

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,10 +1,35 @@
 import { useState } from "react"
 import Button from "./layout/Button"
 
-function ReviewForm() {
-    const [text, setText] = useState('')
+const spanStyle = {
+  display: 'block',
+  fontStyle: 'italic',
+  marginTop: '10px'
+}
 
+function ReviewForm() {
+    // state for input text 
+  const [text, setText] = useState('')
+
+    // state for button (disable)
+    const [btnDisabled, setBtnDisabled] = useState(true)
+
+    // state for validation (characters)
+    const [msg, setMsg] = useState('')
+
+    // function to update the text state (in the input field) and also validate the text characters to be above 20 before button will be enabled
     const textHandler = (e) => {
+      if(text === ''){
+        setBtnDisabled(true) //disbale button
+        setMsg(null) // do not display message
+      } else if(text !== '' && text.trim().length <=20){
+        setMsg('Your review must be above 20 characters') //display message
+        setBtnDisabled(true) //diable button
+      } else {
+        setMsg(null) // do not display message
+        setBtnDisabled(false) //enable button
+      }
+
         setText(e.target.value)
     }
 
@@ -17,10 +42,11 @@ function ReviewForm() {
                 <div className="input-group">
                     <input type="text" value={text} placeholder="write your review here" onChange={textHandler} />
                     
-                    <Button type='submit' variant='secondary'>
+                    <Button type='submit' variant='secondary' isDisabled={btnDisabled}>
                         Submit
                     </Button>
                 </div>
+                {msg && (<span className="message" style={spanStyle}>{msg}</span>)}
             </form>
         </div>
       </div>
