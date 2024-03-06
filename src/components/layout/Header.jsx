57cb346a621b984848d13afd6faf9156ef0ef80a
Ayodele Aransiola <leomdevs@gmail.com>
2024-03-06 13:50:48 +0100

    style: add backround to header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,14 @@
 
+const header = {
+  backgroundColor: '#ff6a95',
+  color: '#fff'
+}
 
 function Header({text}) {
+
   return (
     <>
-      <header>
+      <header style={header} className="head">
         <h3>{text}</h3>
       </header>
     </>
